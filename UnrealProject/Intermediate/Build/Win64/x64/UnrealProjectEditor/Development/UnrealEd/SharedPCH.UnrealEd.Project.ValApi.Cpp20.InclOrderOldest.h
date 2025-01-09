3e7439f5f6647fc6d09bef810e92c1987df71f52
Samuel Fines (U2462878) <U2462878@unimail.hud.ac.uk>
2025-01-09 16:55:40 +0000

    Added in health/energy bars

--- UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h
+++ UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h
@@ -1,3 +1,3 @@
 // PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/U2462878/OneDrive - University of Huddersfield/Documents/Git/game-jam-1-a/UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h"
+#include "C:/Users/U2462878/Git/game-jam-1/UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h"
 #include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
